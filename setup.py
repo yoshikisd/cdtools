6d2f92584d5ed3c91023fcfee67822ff820e3edd
Abe Levitan <alevitan@mit.edu>
2019-03-20 09:35:55 -0400

    Add start of structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CDTools",
+    version="0.0.1",
+    author="Abe Levitan, Madelyn Cain",
+    author_email="alevitan@mit.edu",
+    description="Coherent Diffraction Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.mit.edu/scattering/CDTools.git",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "pytorch",
+        "h5py"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
