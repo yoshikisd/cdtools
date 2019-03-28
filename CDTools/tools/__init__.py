cbb8d36187727fe6f8d4532e3f3b6e58aacab938
Maddie Cain <maddiecain10@gmail.com>
2019-03-27 21:59:05 -0400

    fixed merge conflict

--- CDTools/tools/__init__.py
+++ CDTools/tools/__init__.py
@@ -3,11 +3,8 @@ from __future__ import division, print_function, absolute_import
 from CDTools.tools import cmath
 from CDTools.tools import losses
 from CDTools.tools import data
-<<<<<<< HEAD
 from CDTools.tools import image_processing
 from CDTools.tools import initializers
 from CDTools.tools import losses
 from CDTools.tools import projectors
 from CDTools.tools import propagators
-=======
->>>>>>> bad75422717fd0eecfe1a136fddc2b96828a3321
