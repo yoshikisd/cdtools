540cc997b5f91170f56de9ddb920b9c2ef1e7e6b
Anastasiia Kutakh <anastasiiakutakh@C02FK59HMD6M.tld>
2021-08-20 11:52:23 -0400

    .

--- CDTools/models/polarized_fancy_ptycho.py
+++ CDTools/models/polarized_fancy_ptycho.py
@@ -21,13 +21,13 @@ class PolarizedFancyPtycho(FancyPtycho):
                  detector_slice=None,
                  surface_normal=np.array([0.,0.,1.]),
                  min_translation = t.Tensor([0,0]),
-                 background = None, translation_offsets=None, 
+                 background = None, translation_offsets=None,
                  polarizer_offsets=None, analyzer_offsets=None,
                  polarizer_scale=1, analyzer_scale=1,  mask=None,
                  weights = None, translation_scale = 1, saturation=None,
                  probe_support = None, obj_support=None, oversampling=1,
                  loss='amplitude mse',units='um'):
-        
+
         super(FancyPtycho, self).__init__(wavelength, detector_geometry,
                  probe_basis,
                  probe_guess, obj_guess,
@@ -48,29 +48,55 @@ class PolarizedFancyPtycho(FancyPtycho):
             self.analyzer_offsets = None
         else:
             self.analyzer_offsets = t.nn.Parameter(t.tensor(analyzer_offsets).to(dtype=t.float32)) / analyzer_scale
-        
+
+        self.polarizer = polarizer
+        self.analyzer = analyzer
+        probe_guess = t.tensor(probe_guess, dtype=t.complex64)
+        if probe_guess.dim() > 4:
+            self.probe_norm = 1 * t.max(t.abs(probe_guess[0]))
+        else:
+            self.probe_norm = 1 * t.max(t.abs(probe_guess))
+
+        self.probe = t.nn.Parameter(probe_guess / self.probe_norm)
+
     @classmethod
     def from_dataset(cls, dataset, probe_size=None, randomize_ang=0, padding=0, n_modes=1, dm_rank=None, translation_scale = 1, saturation=None, probe_support_radius=None, propagation_distance=None, restrict_obj=-1, scattering_mode=None, oversampling=1, auto_center=False, opt_for_fft=False, loss='amplitude mse', units='um', left_polarized=True):
-        
+
         model = FancyPtycho.from_dataset(dataset, probe_size=None, randomize_ang=0, padding=0, n_modes=1, dm_rank=None, translation_scale = 1, saturation=None, probe_support_radius=None, propagation_distance=None, scattering_mode=None, oversampling=1, auto_center=False, opt_for_fft=False, loss='amplitude mse', units='um')
 
 
-        # Mutate the class to its subclass 
+        # Mutate the class to its subclass
         model.__class__ = cls
 
         if left_polarized:
             x = 1j
         else:
             x = -1j
-        model.probe.data = t.stack((model.probe.data.to(dtype=t.cfloat), x * model.probe.data.to(dtype=t.cfloat)), dim=-3)
-        obj = t.stack((model.obj.data, model.obj.data), dim=-3)
-        model.obj.data = t.stack((obj, obj), dim=-4)        
 
+        # if probe_size is None:
+        #     probe = tools.initializers.SHARP_style_probe(dataset, probe_shape, det_slice, propagation_distance=propagation_distance, oversampling=oversampling)
+        # else:
+        #     probe = tools.initializers.gaussian_probe(dataset, probe_basis, probe_shape, probe_size, propagation_distance=propagation_distance)
+        probe = model.probe.detach()
+
+        probe = t.stack((probe, probe * x), dim=-3)
+        probe_max = t.max(t.abs(probe))
+        probe_stack = [0.01 * probe_max * t.rand(probe.shape, dtype=probe.dtype) for i in range(n_modes - 1)]
+        probe = t.stack([probe, ] + probe_stack)
+
+        model.probe.data = probe
+        # obj = t.stack((model.obj.data, model.obj.data), dim=-3)
+        # model.obj.data = t.stack((obj.data, obj.data), dim=-4)
+        # obj = t.exp(1j * randomize_ang * (t.rand(obj_size)-0.5))
+        obj = model.obj.detach()
+        obj = t.stack((obj, obj), dim=-3)
+        obj = t.stack((obj, obj), dim=-4)
+        model.obj.data = obj
         # tensor vs tensor.data
         return model
 
     polarizers = [tools.polarization.generate_linear_polarizer(i * 45) for i in range(3)]
-    
+
     # WHAT IS INDEX?
     def interaction(self, index, translations, polarizer, analyzer, test=False):
 
@@ -84,9 +110,9 @@ class PolarizedFancyPtycho(FancyPtycho):
         if self.translation_offsets is not None:
             pix_trans += self.translation_scale * self.translation_offsets[index]
 
-             
+
         # This restricts the basis probes to stay within the probe support
-        basis_prs = self.probe * self.probe_support[...,:,:] # This makes no sense 
+        basis_prs = self.probe * self.probe_support[...,:,:] # This makes no sense
         # self.probe is an Nx2xXxY stach of probes
 
         # Now we construct the probes for each shot from the basis probes
@@ -106,7 +132,7 @@ class PolarizedFancyPtycho(FancyPtycho):
         analyzed_exit_waves = polarization.apply_linear_polarizer(exit_waves, analyzer)
 
         return analyzed_exit_waves
-    
+
 
     def vectorial_wavefields(wavefields, func, *args, **kwargs):
         wavefields_x = wavefields[..., 0, :, :, :]
@@ -122,7 +148,7 @@ class PolarizedFancyPtycho(FancyPtycho):
     def backward_propagator(self, wavefields):
         return tools.propagators.inverse_far_field(wavefields)
 
-    
+
     def measurement(self, wavefields):
         wavefields_x = wavefields[..., 0, :, :]
         wavefields_y = wavefields[..., 1, :, :]
@@ -145,15 +171,15 @@ class PolarizedFancyPtycho(FancyPtycho):
 
     # Note: No "loss" function is defined here, because it is added
     # dynamically during object creation in __init__
-    
+
     def to(self, *args, **kwargs):
         super(PolarizedFancyPtycho, self).to(*args, **kwargs)
 
-        
+
     def sim_to_dataset(self, args_list):
         # In the future, potentially add more control
         # over what metadata is saved (names, etc.)
-        
+
         # First, I need to gather all the relevant data
         # that needs to be added to the dataset
         entry_info = {'program_name': 'CDTools',
@@ -166,16 +192,16 @@ class PolarizedFancyPtycho(FancyPtycho):
         ysurfacevec = np.cross(surface_normal, xsurfacevec)
         ysurfacevec /= np.linalg.norm(ysurfacevec)
         orientation = np.array([xsurfacevec, ysurfacevec, surface_normal])
-        
+
         sample_info = {'description': 'A simulated sample',
                        'orientation': orientation}
 
-        
+
         detector_geometry = self.detector_geometry
         mask = self.mask
         wavelength = self.wavelength
         indices, translations = args_list
-        
+
         # Then we simulate the results
         data = self.forward(indices, translations)
 
@@ -187,14 +213,14 @@ class PolarizedFancyPtycho(FancyPtycho):
                                  detector_geometry=detector_geometry,
                                  mask=mask)
 
-    
+
     def corrected_translations(self, dataset):
         translations = dataset.translations.to(dtype=t.float32,device=self.probe.device)
         t_offset = tools.interactions.pixel_to_translations(self.probe_basis,self.translation_offsets*self.translation_scale,surface_normal=self.surface_normal)
         return translations + t_offset
 
 
-    
+
     def get_rhos(self):
         # If this is the general unified mode model
         if self.weights.dim() >= 2:
@@ -205,18 +231,18 @@ class PolarizedFancyPtycho(FancyPtycho):
         else:
             return np.array([np.eye(self.probe.shape[0])]*self.weights.shape[0],
                             dtype=np.complex64)
-        
+
     def tidy_probes(self, normalization=1, normalize=False):
         """Tidies up the probes
-        
+
         What we want to do here is use all the information on all the probes
         to calculate a natural basis for the experiment, and update all the
         density matrices to operate in that updated basis
-        
+
         """
-        
+
         # First we treat the purely incoherent case
-        
+
         # I don't love this pattern of using an if statement with a return
         # to catch this case, but because it's so much simpler than the
         # unified mode case I think it's appropriate
@@ -228,11 +254,11 @@ class PolarizedFancyPtycho(FancyPtycho):
             return
 
         # This is for the unified mode case
-        
+
         # Note to future: We could probably do this more cleanly with an
         # SVD directly on the Ws matrix, instead of an eigendecomposition
         # of the rho matrix.
-        
+
         rhos = self.get_rhos()
         overall_rho = np.mean(rhos,axis=0)
         probe = self.probe.detach().cpu().numpy()
@@ -248,7 +274,7 @@ class PolarizedFancyPtycho(FancyPtycho):
         ortho_probes *= np.sqrt(normalization)
 
         dm_rank = self.weights.shape[1]
-        
+
         new_Ws = []
         for rho in new_rhos:
             # These are returned from smallest to largest - we want to keep
@@ -263,14 +289,14 @@ class PolarizedFancyPtycho(FancyPtycho):
             # when there are thousands of individual matrices to transform
             # every time this is called.
             w = np.maximum(w,0)
-            
+
             new_Ws.append(np.dot(np.diag(np.sqrt(w)),v.transpose()))
-            
+
         new_Ws = np.array(new_Ws)
 
         self.weights.data = t.as_tensor(new_Ws,
             dtype=self.weights.dtype,device=self.weights.device)
-        
+
         self.probe.data = t.as_tensor(ortho_probes,
             device=self.probe.device,dtype=self.probe.dtype)
 
@@ -287,32 +313,32 @@ class PolarizedFancyPtycho(FancyPtycho):
                 return np.sum(np.abs(ortho_probes.detach().cpu().numpy())**2,axis=0)
             if mode.lower() == 'phase':
                 return np.angle(ortho_probes.detach().cpu().numpy())
-            
+
         probe_matrix = np.zeros([self.probe.shape[0]]*2,
                                 dtype=np.complex64)
         np_probes = self.probe.detach().cpu().numpy()
         for i in range(probe_matrix.shape[0]):
             for j in range(probe_matrix.shape[0]):
                 probe_matrix[i,j] = np.sum(np_probes[i]*np_probes[j].conj())
-        
+
 
         weights = self.weights.detach().cpu().numpy()
-        
+
         probe_intensities = np.sum(np.tensordot(weights,probe_matrix,axes=1)*
                                    weights.conj(),axis=2)
 
         # Imaginary part is already essentially zero up to rounding error
         probe_intensities = np.real(probe_intensities)
-        
+
         values = np.sum(probe_intensities,axis=1)
         if mode.lower() == 'amplitude' or mode.lower() == 'root_sum_intensity':
             cmap = 'viridis'
         else:
             cmap = 'twilight'
-            
+
         p.plot_nanomap_with_images(self.corrected_translations(dataset), get_probes, values=values, fig=fig, units=self.units, basis=self.probe_basis, nanomap_colorbar_title='Total Probe Intensity',cmap=cmap,**kwargs),
 
-        
+
     plot_list = [
         ('',
          lambda self, fig, dataset: self.plot_wavefront_variation(dataset,fig=fig,mode='root_sum_intensity',image_title='Root Summed Probe Intensities',image_colorbar_title='Square Root of Intensity'),
@@ -333,7 +359,7 @@ class PolarizedFancyPtycho(FancyPtycho):
         ('% Power in Top Mode (only accurate after tidy_probes)',
          lambda self, fig, dataset: p.plot_nanomap(self.corrected_translations(dataset), analysis.calc_top_mode_fraction(self.get_rhos()), fig=fig,units=self.units),
          lambda self: len(self.weights.shape) >=2),
-        ('Object Amplitude', 
+        ('Object Amplitude',
          lambda self, fig: p.plot_amplitude(self.obj, fig=fig, basis=self.probe_basis,units=self.units)),
         ('Object Phase',
          lambda self, fig: p.plot_phase(self.obj, fig=fig, basis=self.probe_basis,units=self.units)),
@@ -343,7 +369,7 @@ class PolarizedFancyPtycho(FancyPtycho):
          lambda self, fig: plt.figure(fig.number) and plt.imshow(self.background.detach().cpu().numpy()**2))
     ]
 
-    
+
     def save_results(self, dataset):
         basis = self.probe_basis.detach().cpu().numpy()
         translations = self.corrected_translations(dataset).detach().cpu().numpy()
@@ -352,7 +378,7 @@ class PolarizedFancyPtycho(FancyPtycho):
         obj = self.obj.detach().cpu().numpy()
         background = self.background.detach().cpu().numpy()**2
         weights = self.weights.detach().cpu().numpy()
-        
+
         return {'basis':basis, 'translation':translations,
                 'probe':probe,'obj':obj,
                 'background':background,
