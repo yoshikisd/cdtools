8ef34116041107b60a33cdae1707e570d835e633
Abe Levitan <allevitan@gmail.com>
2023-11-08 13:09:54 +0100

    Whoops I forgot some files in the last commit

--- src/cdtools/datasets/polarization_swept_ptycho_2d_dataset.py
+++ src/cdtools/datasets/polarization_swept_ptycho_2d_dataset.py
@@ -1,40 +1,48 @@
-import numpy as np
 import torch as t
 import h5py
 import pathlib
 from cdtools.datasets import Ptycho2DDataset
 from cdtools.tools import data as cdtdata
-from cdtools.tools import plotting
 
-__all__ = ['PolarizedPtycho2DDataset']
+__all__ = ['PolarizationSweptPtycho2DDataset']
 
 
-class PolarizedPtycho2DDataset(Ptycho2DDataset):
+class PolarizationSweptPtycho2DDataset(Ptycho2DDataset):
     """The standard dataset for a 2D ptychography scan
 
-    Subclasses datasets.Ptycho2DDataset
+    Subclasses datasets.CDataset
 
     This class loads and saves 2D ptychography scan data from .cxi files.
     It should save and load files compatible with most reconstruction
     programs, although it is only tested against SHARP.
     """
-
-    def __init__(self, translations, polarizer_angles, analyzer_angles, patterns, axes=None, *args, **kwargs):
+    def __init__(self, translations, patterns, polarization_indices,
+                 polarization_states,
+                 *args, **kwargs):
         """The __init__ function allows construction from python objects.
 
         The detector_geometry dictionary is defined to have the
         entries defined by the outputs of data.get_detector_geometry.
 
+        Note that the created dataset object will not copy the data in the
+        patterns parameter in order to avoid doubling the memory requiement
+        for large datasets.
 
         Parameters
         ----------
-        translations : array (nx3 t.tensor when polarized=True)
+        translations : array
             An nx3 array containing the probe translations at each scan point
         patterns : array
-            An nxmxl array containing the full stack of measured diffraction patterns
+            An nxmxl array containing the full stack of measured diffraction
+            patterns
+        polarization_indices : array(int)
+            A length-n array containing the index of the polarization state
+            attached to each pattern
+        polarization_states : array
+            An mx2 array, where m is the number of polarization indices,
+            encoding the polarzation state associated with each index
         axes : list(str)
-            A
-             of names for the axes of the probe translations
+            A list of names for the axes of the probe translations
         entry_info : dict
             A dictionary containing the entry_info metadata
         sample_info : dict
@@ -50,15 +58,16 @@ class PolarizedPtycho2DDataset(Ptycho2DDataset):
         background : array
             An initial guess for the not-previously-subtracted
             detector background
+        intensities : array
+            A list of measured shot-to-shot intensities
         """
         
-        super(PolarizedPtycho2DDataset,self).__init__(translations, patterns,
-                                             *args, **kwargs)
+        super().__init__(translations, patterns,
+                                                      *args, **kwargs)
         
-        
-        self.polarizer = t.tensor(polarizer_angles, dtype=t.float32)
-        self.analyzer = t.tensor(analyzer_angles, dtype=t.float32)
-        
+        self.polarization_indices = t.tensor(polarization_indices, dtype=t.int32)
+        self.polarization_states = t.tensor(polarization_states, dtype=t.float32)
+
         
     def _load(self, index):
         """ Internal function to load data
@@ -96,7 +105,7 @@ class PolarizedPtycho2DDataset(Ptycho2DDataset):
              The output pattern or stack of output patterns
         """
         return ((index, self.translations[index],
-                 self.polarizer[index], self.analyzer[index]),
+                 self.polarization_indices[index]),
                 self.patterns[index])
 
     def to(self, *args, **kwargs):
@@ -107,19 +116,18 @@ class PolarizedPtycho2DDataset(Ptycho2DDataset):
 
         Accepts the same parameters as torch.Tensor.to
         """
-        super(PolarizedPtycho2DDataset, self).to(*args, **kwargs)
-        self.polarizer = self.polarizer.to(*args, **kwargs)
-        self.analyzer = self.analyzer.to(*args, **kwargs)
+        super().to(*args, **kwargs)
+        self.polarization_states = self.polarization_states.to(*args, **kwargs)
 
 
     # It sucks that I can't reuse the base factory method here,
     # perhaps there is a way but I couldn't figure it out.
     @classmethod
     def from_cxi(cls, cxi_file, cut_zeros=True):
-        """Generates a new PolarizedPtycho2DDataset from a .cxi file directly
+        """Generates a new PolarizationSweptPtycho2DDataset from a .cxi file 
 
-        This generates a new PolarizedPtycho2DDataset from a .cxi file storing
-        a 2D ptychography scan.
+        This generates a new PolarizationSweptPtycho2DDataset from a .cxi file
+        storing the 2D ptychography scan.
 
         Parameters
         ----------
@@ -130,7 +138,7 @@ class PolarizedPtycho2DDataset(Ptycho2DDataset):
 
         Returns
         -------
-        dataset : PolarizedPtycho2DDataset
+        dataset : PolarizationSweptPtycho2DDataset
             The constructed dataset object
         """
 
@@ -146,20 +154,20 @@ class PolarizedPtycho2DDataset(Ptycho2DDataset):
         dataset.__class__ = cls
 
         # Now, we save out the polarizer and analyzer states
-        polarizer = cdtdata.get_shot_to_shot_info(cxi_file, 'polarizer_angle')
-        analyzer = cdtdata.get_shot_to_shot_info(cxi_file, 'analyzer_angle')
+        polarization_indices = cdtdata.get_shot_to_shot_info(cxi_file, 'polarization_indices')
+        polarization_states = cdtdata.get_entry_info(cxi_file, 'polarization_states')
 
-        dataset.analyzer = t.tensor(analyzer, dtype=t.float32)
-        dataset.polarizer = t.tensor(polarizer, dtype=t.float32)
+        dataset.polarization_indices = t.tensor(polarization_indices, dtype=t.int32)
+        dataset.polarization_states = t.tensor(polarization_states, dtype=t.complex64)
         
         return dataset
 
     def to_cxi(self, cxi_file, polarized=False):
-        """Saves out a PolarizedPtycho2DDataset as a .cxi file
+        """Saves out a PolarizationSweptPtycho2DDataset as a .cxi file
 
         This function saves all the compatible information in a
-        PolarizedPtycho2DDataset object into a .cxi file. This saved .cxi file
-        should be compatible with any standard .cxi file based
+        PolarizationSweptPtycho2DDataset object into a .cxi file. This saved
+        .cxi file should be compatible with any standard .cxi file based
         reconstruction tool, such as SHARP.
 
         Parameters
@@ -174,45 +182,10 @@ class PolarizedPtycho2DDataset(Ptycho2DDataset):
                 return self.to_cxi(f)
 
         # This saves the translations, patterns, etc.
-        super(PolarizedPtycho2DDataset, self).to_cxi(cxi_file)
+        super().to_cxi(cxi_file)
 
-        # Now, we save out the polarizer and analyzer states
-        cdtdata.add_shot_to_shot_info(cxi_file, self.polarizer,
-                                      'polarizer_angle')
-        cdtdata.add_shot_to_shot_info(cxi_file, self.analyzer,
-                                      'analyzer_angle')
-
-    def inspect(self, logarithmic=True, units='um'):
-        """Launches an interactive plot for perusing the data
-
-        This launches an interactive plotting tool in matplotlib that
-        shows the spatial map constructed from the integrated intensity
-        at each position on the left, next to a panel on the right that
-        can display a base-10 log plot of the detector readout at each
-        position.
-        """
-
-
-        def get_images(idx):
-            inputs, output = self[idx]
-            meas_data = output.detach().cpu().numpy()
-            if hasattr(self, 'mask') and self.mask is not None:
-                mask = self.mask.detach().cpu().numpy()
-            else:
-                mask = 1
-                
-            if logarithmic:
-                return np.log(meas_data) / np.log(10) * mask
-            else:
-                return meas_data * mask
-
-        translations = self.translations.detach().cpu().numpy()
-        nanomap_values = (self.mask.to(t.float32) * self.patterns).sum(dim=(1,2)).detach().cpu().numpy()
-
-        if logarithmic:
-            cbar_title='Log Base 10 of Diffraction Intensity'
-        else:
-            cbar_title='Diffraction Intensity'
-        
-        plotting.plot_nanomap_with_images(self.translations.detach().cpu(), get_images, values=nanomap_values, nanomap_units=units, image_title='Diffraction Pattern', image_colorbar_title=cbar_title)
-
+        # Now, we save out the polarization states
+        cdtdata.add_shot_to_shot_info(cxi_file, self.polarization_indices,
+                                      'polarization_indices')
+        cdtdata.add_entry_info(cxi_file, self.polarization_states,
+                                      'polarization_states')
