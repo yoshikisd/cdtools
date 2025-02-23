fc40b77043b96e068122507ea40aa0e3c0331f8f
yoshikisd <yoshikisd@outlook.com>
2025-02-23 20:48:54 +0000

    Reworked fancy ptycho example for multi GPU testing with DDP

--- examples/fancy_ptycho_multi_gpu_ddp.py
+++ examples/fancy_ptycho_multi_gpu_ddp.py
@@ -1,44 +1,131 @@
 import cdtools
 from matplotlib import pyplot as plt
 
-filename = 'example_data/lab_ptycho_data.cxi'
-dataset = cdtools.datasets.Ptycho2DDataset.from_cxi(filename)
+# To use multiple GPUs, we need to import a few additional packages
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group, barrier
+import torch.multiprocessing as mp
 
-# FancyPtycho is the workhorse model
-model = cdtools.models.FancyPtycho.from_dataset(
-    dataset,
-    n_modes=3, # Use 3 incoherently mixing probe modes
-    oversampling=2, # Simulate the probe on a 2xlarger real-space array
-    probe_support_radius=120, # Force the probe to 0 outside a radius of 120 pix
-    propagation_distance=5e-3, # Propagate the initial probe guess by 5 mm
-    units='mm', # Set the units for the live plots
-    obj_view_crop=-50, # Expands the field of view in the object plot by 50 pix
-)
+# While not strictly necessary, it's super useful to have in the event
+# the computation hangs by defining a timeout period. 
+import datetime 
+timeout = datetime.timedelta(seconds=60)   # Terminate if things hang for 60s.
 
-device = 'cuda'
-model.to(device=device)
-dataset.get_as(device=device)
+# We will need to specify what multiprocessing backend we want to use.
+# PyTorch supports a few backends (gloo, MPI, NCCL). We will use NCCL, or
+# NVIDIA Collective Communications Library, as it's the fastest one.
+#
+# It's also the only one that works with the current multi-GPU implementation...
+BACKEND = 'nccl'
 
-# The learning rate parameter sets the alpha for Adam.
-# The beta parameters are (0.9, 0.999) by default
-# The batch size sets the minibatch size
-for loss in model.Adam_optimize(50, dataset, lr=0.02, batch_size=10):
-    print(model.report())
-    # Plotting is expensive, so we only do it every tenth epoch
-    if model.epoch % 10 == 0:
-        model.inspect(dataset)
+# We need to wrap the script inside a function in order to use "mp.spawn"
+# which will help distribute the work to multiple GPUs
+#
+# In fancier terms, we will use mp.spawn to create several processes
+# that will work on the model using N-number of GPUs, (a.k.a., 'WORLD_SIZE')
+# Each process will be given to one GPU that's assigned a number called 
+# a RANK (which ranges from 0 to WORLD_SIZE-1).
+def multi_gpu_reconstruct(rank: int, 
+                          world_size: int):
+    """Perform the reconstruction using several GPUs
+    Parameters:
+        rank: int
+            The rank of the GPU to be used. Value should be within
+            [0, world_size-1]
 
-# It's common to chain several different reconstruction loops. Here, we
-# started with an aggressive refinement to find the probe, and now we
-# polish the reconstruction with a lower learning rate and larger minibatch
-for loss in model.Adam_optimize(50, dataset,  lr=0.005, batch_size=50):
-    print(model.report())
-    if model.epoch % 10 == 0:
-        model.inspect(dataset)
+        world_size: int
+            The total number of GPUs to use
+    """
+    # We need to initialize the distributed process group
+    # before calling any other method
+    init_process_group(backend=BACKEND,
+                       rank=rank,
+                       world_size=world_size,
+                       timeout=timeout)
+    
+    filename = 'example_data/lab_ptycho_data.cxi'
+    dataset = cdtools.datasets.Ptycho2DDataset.from_cxi(filename)
 
-# This orthogonalizes the recovered probe modes
-model.tidy_probes()
+    model = cdtools.models.FancyPtycho.from_dataset(
+        dataset,
+        n_modes=3,
+        oversampling=2, 
+        probe_support_radius=120, 
+        propagation_distance=5e-3,
+        units='mm', 
+        obj_view_crop=-50,
+    )
 
-model.inspect(dataset)
-model.compare(dataset)
-plt.show()
+    # We need to adjust the device string to also indicate which GPU this
+    # process is using
+    device = f'cuda:{rank}'
+    model.to(device=device)
+    dataset.get_as(device=device)
+
+    # We now wrap the model with DistributedDataParallel (DDP), which allows
+    # data parallelism by synchronizing gradients across each copy of the
+    # model in the different GPUs.
+    model = DDP(model,
+                device_ids=[rank],  # Tells DDP which GPU the model lives in
+                output_device=rank, # Tells DDP which GPU to output to
+                find_unused_parameters=True) # TODO: Understand what this is really doing...
+
+    # As a sanity check, we wait for all GPUs to catch up to barrier() before
+    # running optimization
+    barrier()
+    
+
+    # Since our model is now wrapped in DDP, all CDTools methods have to be
+    # called using 'model.module' rather than just 'model'.
+    #
+    # We also need to pass the rank and world_size to Adam_optimize
+    for loss in model.module.Adam_optimize(50, 
+                                           dataset, 
+                                           lr=0.02, 
+                                           batch_size=10,
+                                           rank=rank,
+                                           num_workers=world_size):
+        
+        # We can still perform model.inspect and model.report, but we want
+        # to only let 1 GPU handle plotting/printing rather than get N copies
+        # from all N GPUs.
+        if rank == 0:
+            print(model.module.report())
+        
+        # We set up the model.inspect this way to only let GPU 0 plot and
+        # prevent the other GPUs from running far ahead of GPU 0, which
+        # seems to cause bugs (GPU processes dissapear from nvidia-smi)
+        if model.module..epoch % 10 == 0:
+            if rank == 0:
+                model.module.inspect(dataset)
+            barrier()
+
+
+    # We set up another barrier to make sure all GPUs catch up before
+    # starting another reconstruction loop
+    barrier()
+
+    for loss in model.module.Adam_optimize(50, 
+                                           dataset,  
+                                           lr=0.005, 
+                                           batch_size=50,
+                                           rank=rank,
+                                           num_workers=world_size):
+        if rank == 0:
+            print(model.module.report())
+        
+        if model.epoch % 10 == 0:
+            if rank == 0:
+                model.module.inspect(dataset)
+            barrier()
+
+    # Again, set up another barrier to let all GPUs catch up
+    barrier()
+    
+    model.module.tidy_probes() # TODO: Check how the multi-GPU implementation handles tidying probes.
+
+    # Only let one GPU handle plotting stuff.
+    if rank == 0:
+        model.module.inspect(dataset)
+        model.module.compare(dataset)
+        plt.show()
