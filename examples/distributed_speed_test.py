10e4d747d2b331ac8f0db0cb291799c5ecb777cb
yoshikisd <yoshikisd@outlook.com>
2025-04-28 17:51:12 +0000

    Changed name of multi-gpu speed test. Added gold balls example to the speed test. Added speed-up versus GPU count plot

--- examples/distributed_speed_test.py
+++ examples/distributed_speed_test.py
@@ -13,28 +13,19 @@ import cdtools
 from cdtools.models import CDIModel
 from cdtools.datasets.ptycho_2d_dataset import Ptycho2DDataset
 from cdtools.tools.distributed import distributed
+import torch.multiprocessing as mp
+import torch as t
 from multiprocessing.connection import Connection
 from typing import Tuple, List
 from matplotlib import pyplot as plt
-import torch.multiprocessing as mp
 import time
 import numpy as np
 from copy import deepcopy
 
-# Load the dataset
-filename = r'examples/example_data/lab_ptycho_data.cxi'
-dataset = cdtools.datasets.Ptycho2DDataset.from_cxi(filename)
-
-# Create the model
-model = cdtools.models.FancyPtycho.from_dataset(
-    dataset,
-    n_modes=3,
-    oversampling=2, 
-    probe_support_radius=120, 
-    propagation_distance=5e-3,
-    units='mm', 
-    obj_view_crop=-50,
-)
+#The test to run:
+#   'fancy_ptycho' - Runs reconstruction parameters from examples/fancy_ptycho.py
+#   'gold_balls' - Runs reconstruction parameters from examples/gold_ball_ptycho.py
+TEST = 'fancy_ptycho'
 
 # Multi-GPU supported reconstruction
 def reconstruct(model: CDIModel,
@@ -82,21 +73,39 @@ def reconstruct(model: CDIModel,
         model.to(device=device)
         dataset.get_as(device=device)
 
+
     # Perform reconstructions on either single or multi-GPU workflows.
-    for loss in model.Adam_optimize(100, dataset, lr=0.02, batch_size=10):
-        if rank == 0:
-            print(model.report())
-            t_list.append(time.time() - t_start)
+    if TEST == 'fancy_ptycho':
+        for loss in model.Adam_optimize(50, dataset, lr=0.02, batch_size=40):
+            if rank == 0:
+                print(model.report())
+                t_list.append(time.time() - t_start)
 
-    for loss in model.Adam_optimize(100, dataset, lr=0.005, batch_size=50):
-        if rank == 0:
-            print(model.report())
-            t_list.append(time.time() - t_start)
+        for loss in model.Adam_optimize(25, dataset, lr=0.005, batch_size=40):
+            if rank == 0:
+                print(model.report())
+                t_list.append(time.time() - t_start)
 
-    for loss in model.Adam_optimize(100, dataset, lr=0.001, batch_size=50):
-        if rank == 0:
-            print(model.report())
-            t_list.append(time.time() - t_start)
+        for loss in model.Adam_optimize(25, dataset, lr=0.001, batch_size=40):
+            if rank == 0:
+                print(model.report())
+                t_list.append(time.time() - t_start)
+
+    elif TEST == 'gold_balls':
+        for loss in model.Adam_optimize(20, dataset, lr=0.005, batch_size=50):
+            if rank == 0: 
+                print(model.report())
+                t_list.append(time.time() - t_start)
+
+        for loss in model.Adam_optimize(50, dataset, lr=0.002, batch_size=100):
+            if rank == 0:
+                print(model.report())
+                t_list.append(time.time() - t_start)
+
+        for loss in model.Adam_optimize(100, dataset, lr=0.001, batch_size=100):
+            if rank == 0:
+                print(model.report())
+                t_list.append(time.time() - t_start)
 
     # We need to send the time_history and loss_history through
     # the child connection to the parent (sitting in the name-main block)
@@ -126,11 +135,47 @@ def run_test(world_sizes: int,
         runs: int
             How many repeat reconstructions to perform
     """
+    # Load the dataset and model
+    if TEST == 'fancy_ptycho':
+        filename = 'cdtools/examples/example_data/lab_ptycho_data.cxi'
+    elif TEST == 'gold_balls':
+        filename = 'cdtools/examples/example_data/AuBalls_700ms_30nmStep_3_6SS_filter.cxi'
+
+    dataset = cdtools.datasets.Ptycho2DDataset.from_cxi(filename)
+    
+    if TEST == 'fancy_ptycho':
+        model = cdtools.models.FancyPtycho.from_dataset(
+            dataset,
+            n_modes=3,
+            oversampling=2, 
+            probe_support_radius=120, 
+            propagation_distance=5e-3,
+            units='mm', 
+            obj_view_crop=-50)
+        
+    elif TEST == 'gold_balls':
+        pad = 10
+        dataset.pad(pad)
+        model = cdtools.models.FancyPtycho.from_dataset(
+            dataset,
+            n_modes=3,
+            probe_support_radius=50,
+            propagation_distance=2e-6,
+            units='um',
+            probe_fourier_crop=pad)
+        model.translation_offsets.data += 0.7 * t.randn_like(model.translation_offsets)
+        model.weights.requires_grad = False
+
     # Set up a parent/child connection to get some info from the GPU-accelerated function
     parent_conn, child_conn = mp.Pipe()
     
-    # Execute
-    fig, (ax1,ax2) = plt.subplots(1,2)
+    # Set stuff up for plots
+    fig, (ax1,ax2,ax3) = plt.subplots(1,3)
+
+    # Store the value of the single GPU time
+    time_1gpu = 0
+    std_1gpu = 0
+
     for world_size in world_sizes:
         # Get the GPU IDs to use
         dev_id = device_ids[0:world_size] 
@@ -160,7 +205,8 @@ def run_test(world_sizes: int,
                                     master_addr = 'localhost',
                                     master_port = '8888',
                                     timeout=300,
-                                    pipe=child_conn)
+                                    pipe=child_conn,
+                                    nccl_p2p_disable=True)
                 while parent_conn.poll():
                     final_time, loss_history = parent_conn.recv()
                     time_list.append(final_time)
@@ -172,11 +218,22 @@ def run_test(world_sizes: int,
         loss_mean = np.array(loss_hist_list).mean(axis=0)
         loss_std = np.array(loss_hist_list).std(axis=0)
 
+        # If a single GPU is used, store the time
+        if world_size == 1:
+            time_1gpu = time_mean[-1]
+            std_1gpu = time_std[-1]
+
+        # Calculate the speed-up relative to using a single GPU
+        speed_up_mean = time_1gpu / time_mean[-1] 
+        speed_up_std = speed_up_mean * \
+            np.sqrt((std_1gpu/time_1gpu)**2 + (time_std[-1]/time_mean[-1])**2)
+
         # Add another plot
         ax1.errorbar(time_mean, loss_mean, yerr=loss_std, xerr=time_std,
                     label=f'{world_size} GPUs')
         ax2.errorbar(np.arange(0,loss_mean.shape[0]), loss_mean, yerr=loss_std,
                     label=f'{world_size} GPUs')
+        ax3.errorbar(world_size, speed_up_mean, yerr=speed_up_std, fmt='o')
         
     # Plot
     fig.suptitle(f'Multi-GPU performance test | {runs} runs performed')
@@ -184,20 +241,24 @@ def run_test(world_sizes: int,
     ax1.set_xscale('linear')
     ax2.set_yscale('log')
     ax2.set_xscale('linear')
+    ax3.set_yscale('linear')
+    ax3.set_xscale('linear')
     ax1.legend()
     ax2.legend()
     ax1.set_xlabel('Time (min)')
     ax1.set_ylabel('Loss')
     ax2.set_xlabel('Epochs')
+    ax3.set_xlabel('Number of GPUs')
+    ax3.set_ylabel('Speed-up relative to single GPU')
     plt.show()
 
 # This will execute the multi_gpu_reconstruct upon running this file
 if __name__ == '__main__':
     # Define the number of GPUs to use.
-    world_sizes = [1, 2, 4] 
+    world_sizes = [1, 2, 4, 6] 
 
     # Define which GPU IDs to use
-    device_ids = [7, 6, 5, 4]
+    device_ids = [7, 6, 5, 4, 3, 2, 1]
 
     # How many reconstruction runs to perform for statistics
     runs = 3
