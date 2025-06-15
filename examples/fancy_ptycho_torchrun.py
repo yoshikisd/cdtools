588b1504b11a8e56b13b2dbd5d1dc7ccaba11ee1
yoshikisd <yoshikisd@outlook.com>
2025-06-15 05:55:35 +0000

    Revert "Fixed bug in CDatasets which uses cuda:0 when t.cuda.set_device is utilized"
    This reverts commit 3b17f10a862e374b6a6903e3cae23b77b9eb979b.

--- examples/fancy_ptycho_torchrun.py
+++ examples/fancy_ptycho_torchrun.py
@@ -1,10 +1,7 @@
 import cdtools
 from matplotlib import pyplot as plt
-import os
-import torch as t
 
 filename = 'example_data/lab_ptycho_data.cxi'
-#filename = 'cdtools/examples/example_data/lab_ptycho_data.cxi'
 dataset = cdtools.datasets.Ptycho2DDataset.from_cxi(filename)
 
 # FancyPtycho is the workhorse model
@@ -17,8 +14,8 @@ model = cdtools.models.FancyPtycho.from_dataset(
     units='mm', # Set the units for the live plots
     obj_view_crop=-50, # Expands the field of view in the object plot by 50 pix
 )
-#t.cuda.set_device(5)
-device = 'cuda'
+
+device = f'cuda:{model.rank}'
 model.to(device=device)
 dataset.get_as(device=device)
 
--- examples/single_to_multi_gpu.py
+++ examples/single_to_multi_gpu.py
@@ -14,24 +14,26 @@ torchrun --nnodes=1 --nproc_per_node=4 single_to_multi_gpu.py
 """
 import os
 import datetime
-import torch.distributed as dist
 import torch as t
-
+import torch.distributed as dist
 
 
 if __name__ == '__main__':
     # If this script is called by torchrun, several environment
-    # variables are created that we need to store as variables
+    # variables should be visible that are needed to initiate the 
+    # process group.
     rank = int(os.environ.get('RANK'))
     world_size = int(os.environ.get('WORLD_SIZE'))
-
     os.environ['NCCL_P2P_DISABLE'] = str(int(True))
-    t.cuda.set_device(rank)
-    timeout = datetime.timedelta(seconds=300)
+    os.environ['CUDA_VISIBLE_DEVICE'] = str(rank)
+
+    timeout = datetime.timedelta(seconds=30)
 
     # Start up the process group (needed so the different
     # subprocesses can talk with each other)
     dist.init_process_group(backend='nccl',
+                            rank=rank,
+                            world_size=world_size,
                             timeout=timeout)
     
     try:     
