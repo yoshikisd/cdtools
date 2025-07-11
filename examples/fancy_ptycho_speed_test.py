54691fb7821f5f402889662bf636154f9b03f587
yoshikisd <yoshikisd@outlook.com>
2025-07-11 20:25:04 +0000

    Created speed test decorator, changed speed test environ variables, and cleaned up documentation in distributed

--- examples/fancy_ptycho_speed_test.py
+++ examples/fancy_ptycho_speed_test.py
@@ -1,59 +1,44 @@
 import cdtools
-import time
-import pickle
-import os
+
+@cdtools.tools.distributed.report_speed_test
+def main():
+    filename = 'example_data/lab_ptycho_data.cxi'
+    dataset = cdtools.datasets.Ptycho2DDataset.from_cxi(filename)
+
+    # FancyPtycho is the workhorse model
+    model = cdtools.models.FancyPtycho.from_dataset(
+        dataset,
+        n_modes=3, # Use 3 incoherently mixing probe modes
+        oversampling=2, # Simulate the probe on a 2xlarger real-space array
+        probe_support_radius=120, # Force the probe to 0 outside a radius of 120 pix
+        propagation_distance=5e-3, # Propagate the initial probe guess by 5 mm
+        units='mm', # Set the units for the live plots
+        obj_view_crop=-50, # Expands the field of view in the object plot by 50 pix
+    )
+
+    device = 'cuda'
+    model.to(device=device)
+    dataset.get_as(device=device)
 
 
-# Script is intended to be called by distributed_speed_test.py.
-# We need to know which trial number this script is running
-TRIAL_NUMBER = int(os.environ.get('CDTOOLS_TRIAL_NUMBER'))
+    for loss in model.Adam_optimize(50, dataset, lr=0.02, batch_size=40):
+        if model.rank == 0:
+            print(model.report())
 
-filename = 'example_data/lab_ptycho_data.cxi'
-dataset = cdtools.datasets.Ptycho2DDataset.from_cxi(filename)
+    for loss in model.Adam_optimize(25, dataset,  lr=0.005, batch_size=40):
+        if model.rank == 0:
+            print(model.report())
 
-# FancyPtycho is the workhorse model
-model = cdtools.models.FancyPtycho.from_dataset(
-    dataset,
-    n_modes=3, # Use 3 incoherently mixing probe modes
-    oversampling=2, # Simulate the probe on a 2xlarger real-space array
-    probe_support_radius=120, # Force the probe to 0 outside a radius of 120 pix
-    propagation_distance=5e-3, # Propagate the initial probe guess by 5 mm
-    units='mm', # Set the units for the live plots
-    obj_view_crop=-50, # Expands the field of view in the object plot by 50 pix
-)
+    for loss in model.Adam_optimize(25, dataset,  lr=0.001, batch_size=40):
+        if model.rank == 0:
+            print(model.report())
 
-device = 'cuda'
-model.to(device=device)
-dataset.get_as(device=device)
+    # This orthogonalizes the recovered probe modes
+    model.tidy_probes()
 
-for loss in model.Adam_optimize(50, dataset, lr=0.02, batch_size=40):
-    if model.rank == 0:
-        print(model.report())
+    return model
 
-for loss in model.Adam_optimize(25, dataset,  lr=0.005, batch_size=40):
-    if model.rank == 0:
-        print(model.report())
 
-for loss in model.Adam_optimize(25, dataset,  lr=0.001, batch_size=40):
-    if model.rank == 0:
-        print(model.report())
-
-# This orthogonalizes the recovered probe modes
-model.tidy_probes()
-
-# Save the model and loss history
-if model.rank == 0:
-    # Set up the file name:
-    file_name = f'speed_test_nGPUs_{model.world_size}_TRIAL_{TRIAL_NUMBER}'
-    # Grab the loss and time history
-    loss_history = model.loss_history
-    time_history = model.loss_times
-    # Store quantities in a dictionary
-    dict = {'loss history':loss_history,
-            'time history':time_history,
-            'nGPUs':model.world_size,
-            'trial':TRIAL_NUMBER}
-    # Save the quantities
-    with open (f'example_loss_data/'+file_name+'.pkl', 'wb') as f:
-        pickle.dump(dict, f)
+if __name__ == '__main__':
+    main()
 
--- src/cdtools/tools/distributed/distributed.py
+++ src/cdtools/tools/distributed/distributed.py
@@ -31,6 +31,8 @@ from ast import literal_eval
 from matplotlib import pyplot as plt
 import pickle
 import numpy as np
+from typing import Callable
+from pathlib import Path
 
 DISTRIBUTED_PATH = os.path.dirname(os.path.abspath(__file__))
 MIN_INT64 = np.iinfo(np.int64).min
@@ -39,6 +41,7 @@ MAX_INT64 = np.iinfo(np.int64).max
 __all__ = ['sync_and_avg_gradients', 
            'run_single_to_multi_gpu',
            'run_single_gpu_script',
+           'report_speed_test',
            'run_speed_test']
 
 
@@ -59,26 +62,47 @@ def sync_and_avg_gradients(model):
 
 def run_single_to_multi_gpu():
     """
-    Runs a single-GPU reconstruction script as a multi-GPU job via torchrun.
+    Runs a single-GPU reconstruction script as a single-node multi-GPU job via torchrun.
     
-    This function can be executed as `cdt-torchrun` in the command line.
-
-    This function is a wrapper over both the single-GPU wrapping sc
+    This function can be executed as a python console script as `cdt-torchrun` and
+    serves as a wrapper over a `torchrun` call to `cdtools.tools.distributed.single_to_multi_gpu`.
     
-    For example, if we have the reconstruction script `reconstruct.py` and want to use
-    4 GPUs, we can write the following:
-
+    In the simplest case, a single-GPU script can be ran as a multi-GPU job using
+    the following `cdt-torchrun` call in the command line
     ```
-    cdt-torchrun --nproc_per_node=4 s script_path=reconstruct.py
+    cdt-torchrun --ngpus=<number of GPUs> YOUR_RECONSTRUCTION_SCRIPT.py
     ```
+    
+    which is equivalent to the following `torchrun` call
+    ```
+    torchrun 
+        --standalone 
+        --nnodes=1 
+        --nproc_per_node=<number of GPUs> 
+        -m cdtools.tools.distributed.single_to_multi_gpu
+        --backend='nccl'
+        --timeout=30
+        --nccl_p2p_disable=1
+        YOUR_RECONSTRUCTION_SCRIPT.py
+    ```
+    
+    With a single node (--nnodes=1), `cdt-torchrun` will launch a given number of subprocesses 
+    equivalent to the number of GPUs specified. This number must be less than or equal to the
+    actual number of GPUs available on your node.
 
     If you want to use specific GPU IDs for reconstructions, you need to set up
     the environment variable `CDTOOLS_GPU_IDS` rather than `CUDA_VISIBLE_DEVICES`. 
     If you wanted to use GPU IDs `1, 3, 4` for example, write:
     
     ```
-    CDTOOLS_GPU_IDS=1,3,4 cdt-torchrun --nnodes=1 --nproc_per_node=3 reconstruct.py
+    CDTOOLS_GPU_IDS=1,3,4 cdt-torchrun --ngpus=3 YOUR_RECONSTRUCTION_SCRIPT.py
     ```
+
+    If additional `torchrun` arguments need to be passed, you may need to make a direct
+    `torchrun` call rather than use `cdt-torchrun`. You may also submit an issue/PR.
+
+    NOTE: `cdt-torchrun` has only been tested using the 'nccl' backend, NCCL peer-to-peer communication
+          disabled, and using 1 node. 
 
     Arguments:
         script_path: str
@@ -122,10 +146,6 @@ def run_single_to_multi_gpu():
                         default=1,
                         choices=[0,1],
                         help='Disable (1) or enable (0) NCCL peer-to-peer communication')
-    parser.add_argument('--seed',
-                        type=int,
-                        default=None,
-                        help='Sets the RNG seed for all devices')
     parser.add_argument('script_path', 
                         type=str, 
                         help='Single GPU script file name (with or without .py extension)')
@@ -139,7 +159,7 @@ def run_single_to_multi_gpu():
                     f'--nnodes={args.nnodes}', 
                     f'--nproc_per_node={args.ngpus}', 
                     '-m',
-                    'cdtools.tools.distributed.single_to_multi_gpu', # Make the call to the single-to-multi-gpu wrapper script
+                    'cdtools.tools.distributed.single_to_multi_gpu', 
                     f'--backend={args.backend}',
                     f'--timeout={args.timeout}',
                     f'--nccl_p2p_disable={args.nccl_p2p_disable}',
@@ -147,36 +167,49 @@ def run_single_to_multi_gpu():
     
     
 def run_single_gpu_script(script_path: str,
-                           backend: str = 'nccl',
-                           timeout: int = 30,
-                           nccl_p2p_disable: bool = True,
-                           seed: int = None):
+                          backend: str = 'nccl',
+                          timeout: int = 30,
+                          nccl_p2p_disable: bool = True,
+                          seed: int = None):
     """
     Wraps single-GPU reconstruction scripts to be ran as a multi-GPU job via
     torchrun calls. 
 
-    This function is intended to be called in a script (say, single_to_multi_gpu.py) 
-    with the following form:
+    `cdtools.tools.distributed.run_single_gpu_script` is intended to be called in a script
+    (e.g., cdtools.tools.distributed.single_to_multi_gpu) with the following form:
 
     ```
+    # multi_gpu_job.py
     import cdtools.tools.distributed as dist
     if __name__ == '__main__':
-        dist.torchrun_single_to_multi_gpu(**kwargs)
+        dist.run_single_to_multi_gpu(script_path='YOUR_RECONSTRUCTION_SCRIPT.py',
+                                     backend='nccl',
+                                     timeout=30,
+                                     nccl_p2p_disable=True)
     ```
     
-    torchrun should then be used to run this script as a distributive job using,
+    `torchrun` should then be used to run this script as a distributive job using,
     for instance:
     
     ```
-    torchrun --nnodes=1 --nproc_per_node=4 single_to_multi_gpu.py
+    torchrun --nnodes=1 --nproc_per_node=<number of GPUs> multi_gpu_job.py
     ```
 
+    `torchrun` will spawn a number of subprocesses equal to the number of GPUs specified 
+    (--nproc_per_node). On each subprocess, `cdtools.tools.distributed.run_single_gpu_script`
+    will set up process groups (lets each GPU communicate with each other) and environment
+    variables necessary for multi-GPU jobs. The single-GPU script will then be ran by
+    each subprocess, where gradient synchronization will be faciliated by
+    `cdtools.tools.distributed.sync_and_avg_gradients` calls from `cdtools.Reconstructors`
+    while data shuffling/loading is handled by `cdtools.Reconstructor.setup_dataloader`.
+
+    
     If you want to use specific GPU IDs for reconstructions, you need to set up
     the environment variable `CDTOOLS_GPU_IDS` rather than `CUDA_VISIBLE_DEVICES`. 
     If you wanted to use GPU IDs `1, 3, 4` for example, write:
     
     ```
-    CDTOOLS_GPU_IDS=1,3,4 torchrun --nnodes=1 --nproc_per_node=3 single_to_multi_gpu.py
+    CDTOOLS_GPU_IDS=1,3,4 torchrun --nnodes=1 --nproc_per_node=<number of GPUs> multi_gpu_job.py
     ```
     
     NOTE: For each subprocess `cdt-torchrun` creates, the environment variable
@@ -214,7 +247,12 @@ def run_single_gpu_script(script_path: str,
     os.environ['NCCL_P2P_DISABLE'] = str(int(nccl_p2p_disable))
     
     ##########   Force each subprocess to see only the GPU ID we assign it  ###########
-    ###################################################################################
+    # Why am I doing this? If this constraint is not imposed, then calling all_reduce will
+    # cause all subprocess Ranks to occupy memory on both their own respective GPUs (normal) 
+    # as well as Rank 0's GPU (not intended behavior). The root cause is not entirely clear 
+    # but there are two ways to avoid this empirically:
+    #   1) Force each subprocesses' CUDA_VISIBLE_DEVICE to be their assigned GPU ids.
+    #   2) Within the reconstruction script, change `device='cuda'` to `device=f'cuda{model.rank}'`
 
     # The GPU rank and world_size is visible as an environment variable through torchrun calls.
     rank = int(os.environ.get('RANK'))
@@ -232,12 +270,11 @@ def run_single_gpu_script(script_path: str,
     os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
 
     ################################  Run the script  #################################
-    ###################################################################################
 
     if rank == 0:
-        print(f'\n[INFO]: Starting up multi-GPU reconstructions with {world_size} GPUs.')
+        print(f'[INFO]: Starting up multi-GPU reconstructions with {world_size} GPUs.')
 
-    # Start up the process group (lets the different subprocesses can talk with each other)
+    # Start up the process group (lets the different subprocesses talk with each other)
     dist.init_process_group(backend=backend, timeout=datetime.timedelta(seconds=timeout))
 
     try:     
@@ -250,7 +287,7 @@ def run_single_gpu_script(script_path: str,
         t.manual_seed(seed)
 
         # Run the single-GPU reconstruction script 
-        script_variables = runpy.run_path(script_path, run_name='__main__')
+        runpy.run_path(script_path, run_name='__main__')
 
         # Let the user know the job is done
         if rank == 0:
@@ -263,15 +300,69 @@ def run_single_gpu_script(script_path: str,
             print(f'[INFO]: Process group terminated. Multi-GPU job complete.')
 
 
+def report_speed_test(func: Callable):
+    """
+    Decorator function which saves the loss-versus-time/epoch history of a 
+    function-wrapped reconstruction script to a pickle dump file. This function
+    is intended to be used for multi-GPU test studies performed with
+    `cdtools.tools.distributed.run_speed_test`, which sets several environment
+    variables specifing the name and directory of the result files to be saved.
+    
+    If the directory specified by `CDTOOLS_SPEED_TEST_RESULT_DIR` does not exist,
+    one will be created in the current directory.
+
+    Parameters:
+        func: Callable
+            The entire reconstruction script wrapped in a function. Within the
+            script, the function must be called with an if-name-main statement.
+            Additionally, the function must return the reconstructed model.
+    """
+    def wrapper():
+        # Figure out how to name the save file and where to save it to
+        # These environment variables are provided by run_speed_test
+        trial_number = int(os.environ.get('CDTOOLS_TRIAL_NUMBER'))
+        save_dir = os.environ.get('CDTOOLS_SPEED_TEST_RESULT_DIR')
+        file_prefix = os.environ.get('CDTOOLS_SPEED_TEST_PREFIX')
+
+        # Check if the save path is valid
+        # Make sure the directory exists; or else create it
+        Path(save_dir).mkdir(parents=False, exist_ok=True)
+
+        # Run the script
+        model = func()
+
+        # Save the model and loss history, but only using the rank 0 process
+        if model.rank == 0:
+            # Set up the file name:
+            file_name = f'{file_prefix}_nGPUs_{model.world_size}_TRIAL_{trial_number}.pkl'
+            # Grab the loss and time history
+            loss_history = model.loss_history
+            time_history = model.loss_times
+            # Store quantities in a dictionary
+            dict = {'loss history':loss_history,
+                    'time history':time_history,
+                    'nGPUs':model.world_size,
+                    'trial':trial_number}
+            
+            # Save the quantities
+            with open (os.path.join(save_dir, file_name), 'wb') as save_file:
+                pickle.dump(dict, save_file)
+            
+            print(f'[INFO]: Saved results to: {file_name}') 
+    return wrapper
+
+
 def run_speed_test(world_sizes: int, 
                    runs: int,
                    script_path: str,
-                   output_dir: str):
+                   output_dir: str,
+                   file_prefix: str = 'speed_test'):
     """
     Executes a reconstruction script `n` x `m` times using `n` GPUs and `m` trials 
     per GPU count using cdt-torchrun.
 
-    This function assumes that
+    If the directory specified by `output_dir` does not exist,
+    one will be created in the current directory.
 
     Parameters:
         world_sizes: list[int]
@@ -282,6 +373,8 @@ def run_speed_test(world_sizes: int,
             Path of the single-gpu reconstruction script.
         output_dir: str
             Directory of the loss-vs-time/epoch data generated for the speed test.
+        file_prefix: str
+            Prefix of the speed test result file names
     """
     # Set stuff up for plots
     fig, (ax1,ax2,ax3) = plt.subplots(1,3)
@@ -291,29 +384,32 @@ def run_speed_test(world_sizes: int,
     std_1gpu = 0
 
     for world_size in world_sizes:
-        # Get the GPU IDs to use
-        #dev_id = device_ids[0:world_size] 
-        #print(f'\nNumber of GPU(s): {world_size} | Using GPU IDs {*dev_id,}')
-
         # Make a list to store the values
         time_list = []
         loss_hist_list = []
 
         for i in range(runs):
-            print(f'Resetting the model...')
-            print(f'Starting run {i+1}/{runs} on {world_size} GPU(s)')
+            print(f'[INFO]: Resetting the model...')
+            print(f'[INFO]: Starting run {i+1}/{runs} on {world_size} GPU(s)')
 
             # The scripts running speed tests need to read the trial number
             # they are on using an environment variable
             os.environ['CDTOOLS_TRIAL_NUMBER'] = str(i)
+            os.environ['CDTOOLS_SPEED_TEST_RESULT_DIR'] = output_dir
+            os.environ['CDTOOLS_SPEED_TEST_PREFIX'] = file_prefix
             
             # Run cdt-torchrun
-            subprocess.run(['cdt-torchrun',
-                            f'--ngpus={world_size}',
-                            f'{script_path}'])
+            try:
+                subprocess.run(['cdt-torchrun',
+                                f'--ngpus={world_size}',
+                                f'{script_path}'],
+                                check=True)
+            except subprocess.CalledProcessError as e:
+                print(e)
 
             print(f'[INFO]: Reconstruction complete. Loading loss results...')
-            with open(os.path.join(output_dir, f'speed_test_nGPUs_{world_size}_TRIAL_{i}.pkl'), 'rb') as f:
+
+            with open(os.path.join(output_dir, f'{file_prefix}_nGPUs_{world_size}_TRIAL_{i}.pkl'), 'rb') as f:
                 results = pickle.load(f)
             time_list.append(results['time history'])
             loss_hist_list.append(results['loss history'])
@@ -358,3 +454,4 @@ def run_speed_test(world_sizes: int,
     ax3.set_xlabel('Number of GPUs')
     ax3.set_ylabel('Speed-up relative to single GPU')
     plt.show()
+    print(f'[INFO]: Multi-GPU speed test completed.')
