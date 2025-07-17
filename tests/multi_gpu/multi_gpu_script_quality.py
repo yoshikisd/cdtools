27f2d54bbc25def213f46452f6999beef78abb73
yoshikisd <yoshikisd@outlook.com>
2025-07-17 04:36:48 +0000

    Linted multi gpu tests and test scripts

--- tests/multi_gpu/multi_gpu_script_quality.py
+++ tests/multi_gpu/multi_gpu_script_quality.py
@@ -1,6 +1,7 @@
 import cdtools
 import os
 
+
 @cdtools.tools.distributed.report_speed_test
 def main():
     filename = os.environ.get('CDTOOLS_TESTING_DATA_PATH')
@@ -9,34 +10,29 @@ def main():
     # FancyPtycho is the workhorse model
     model = cdtools.models.FancyPtycho.from_dataset(
         dataset,
-        n_modes=3, # Use 3 incoherently mixing probe modes
-        oversampling=2, # Simulate the probe on a 2xlarger real-space array
-        probe_support_radius=120, # Force the probe to 0 outside a radius of 120 pix
-        propagation_distance=5e-3, # Propagate the initial probe guess by 5 mm
-        units='mm', # Set the units for the live plots
-        obj_view_crop=-50, # Expands the field of view in the object plot by 50 pix
+        n_modes=3,
+        oversampling=2,
+        probe_support_radius=120,
+        propagation_distance=5e-3,
+        units='mm',
+        obj_view_crop=-50,
     )
 
     device = 'cuda'
     model.to(device=device)
     dataset.get_as(device=device)
 
-    # Remove or comment out plotting statements
     for loss in model.Adam_optimize(50, dataset, lr=0.02, batch_size=40):
         if model.rank == 0 and model.epoch % 10:
             print(model.report())
-
     for loss in model.Adam_optimize(25, dataset,  lr=0.005, batch_size=40):
         if model.rank == 0 and model.epoch % 10:
             print(model.report())
-
     for loss in model.Adam_optimize(25, dataset,  lr=0.001, batch_size=40):
         if model.rank == 0 and model.epoch % 10:
             print(model.report())
 
-    # This orthogonalizes the recovered probe modes
     model.tidy_probes()
-
     return model
 
 
--- tests/multi_gpu/test_multi_gpu.py
+++ tests/multi_gpu/test_multi_gpu.py
@@ -1,15 +1,12 @@
-import cdtools
 import cdtools.tools.distributed as dist
 import pytest
 import os
 
 """
-This file contains several tests that are relevant to running
-multi-GPU operations in CDTools. 
-
-
-
+This file contains several tests that are relevant to running multi-GPU
+operations in CDTools.
 """
+
 
 @pytest.mark.multigpu
 def test_reconstruction_quality(lab_ptycho_cxi,
@@ -17,16 +14,15 @@ def test_reconstruction_quality(lab_ptycho_cxi,
                                 tmp_path,
                                 show_plot):
     """
-    Run a multi-GPU test based on fancy_ptycho_speed_test.py and make 
+    Run a multi-GPU test based on fancy_ptycho_speed_test.py and make
     sure the final reconstructed loss using 2 GPUs is similar to 1 GPU.
 
     This test requires us to have 2 NVIDIA GPUs and makes use of the
     multi-GPU speed test.
 
-    If this test fails, it indicates that the reconstruction quality
-    is getting noticably worse with increased GPU counts. This may 
-    be a symptom of a synchronization/broadcasting issue between the 
-    different GPUs.
+    If this test fails, it indicates that the reconstruction quality is
+    getting noticably worse with increased GPU counts. This may be a symptom
+    of a synchronization/broadcasting issue between the different GPUs.
     """
     # Pass the cxi directory to the reconstruction script
     os.environ['CDTOOLS_TESTING_DATA_PATH'] = lab_ptycho_cxi
@@ -61,16 +57,18 @@ def test_reconstruction_quality(lab_ptycho_cxi,
     single_gpu_loss_max = loss_mean[0] + loss_std[0]
     multi_gpu_loss_min = loss_mean[1] - loss_std[1]
     multi_gpu_loss_max = loss_mean[1] + loss_std[1]
-    has_overlap_loss = min(single_gpu_loss_max, multi_gpu_loss_max)\
-                       > max(single_gpu_loss_min, multi_gpu_loss_min)
+    has_overlap_loss = \
+        min(single_gpu_loss_max, multi_gpu_loss_max)\
+        > max(single_gpu_loss_min, multi_gpu_loss_min)
 
     print(f'Single GPU final loss: {loss_mean[0]} +- {loss_std[0]}')
     print(f'Two GPU final loss: {loss_mean[1]} +- {loss_std[1]}')
-    print(f'Overlap between the mean +- std of the single/multi GPU losses: {has_overlap_loss}')
+    print('Overlap between mean +- std of the single/multi GPU losses: ' +
+          f'{has_overlap_loss}')
 
     assert has_overlap_loss
 
-    # Also make sure that we actually get some kind of speed up with 
+    # Also make sure that we actually get some kind of speed up with
     # multiple GPUs...
     speed_mean = results[2]
     speed_std = results[3]
@@ -79,17 +77,17 @@ def test_reconstruction_quality(lab_ptycho_cxi,
     single_gpu_speed_max = speed_mean[0] + speed_std[0]
     multi_gpu_speed_min = speed_mean[1] - speed_std[1]
     multi_gpu_speed_max = speed_mean[1] + speed_std[1]
-    has_overlap_speed = min(single_gpu_speed_max, multi_gpu_speed_max)\
-                        > max(single_gpu_speed_min, multi_gpu_speed_min)
+    has_overlap_speed = \
+        min(single_gpu_speed_max, multi_gpu_speed_max)\
+        > max(single_gpu_speed_min, multi_gpu_speed_min)
 
     print(f'Single GPU runtime: {speed_mean[0]} +- {speed_std[0]}')
     print(f'Two GPU runtime: {speed_mean[1]} +- {speed_std[1]}')
-    print(f'Overlap between the mean +- std of the single/multi GPU runtimes: {has_overlap_speed}')
+    print('Overlap between the mean +- std of the single/multi GPU runtimes: '
+          + f'{has_overlap_speed}')
 
     assert speed_mean[0] < speed_mean[1]
     assert not has_overlap_speed
 
     # Clear the environment variable we created here
     os.environ.pop('CDTOOLS_TESTING_DATA_PATH')
-
-
