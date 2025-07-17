be6abcda004b00fc4640aa3a0c23fac39ce4935d
yoshikisd <yoshikisd@outlook.com>
2025-07-17 04:11:01 +0000

    Linted single_to_multi_gpu.py

--- src/cdtools/tools/distributed/single_to_multi_gpu.py
+++ src/cdtools/tools/distributed/single_to_multi_gpu.py
@@ -1,48 +1,52 @@
 """
-A wrapper script to run single-GPU reconstruction scripts as
-a multi-GPU job when called by torchrun.
+A wrapper script to run single-GPU reconstruction scripts as a multi-GPU job
+when called by torchrun.
 
-This script is intended to be called by torchrun. It is set
-up so that the group process handling (init and destroy), 
-definition of several environmental variables, and actual
-execution of the single-GPU script are handled by a single 
-call to dist.script_wrapper.
+This script is intended to be called by torchrun. It is set up so that the
+group process handling (init and destroy), definition of several environmental
+variables, and actual execution of the single-GPU script are handled by a
+single call to dist.run_single_gpu_script.
 
-For example, if we have the reconstruction script `reconstruct.py` and want to use
-4 GPUs, we can write the following:
+For example, if we have the reconstruction script `reconstruct.py` and want to
+use 4 GPUs, we can write the following:
 
 ```
-torchrun --nnodes=1 --nproc_per_node=4 single-to-multi-gpu.py --script_path=reconstruct.py
+torchrun
+    --nnodes=1
+    --nproc_per_node=4
+    single-to-multi-gpu.py
+    --script_path=reconstruct.py
 ```
 """
 import cdtools.tools.distributed as dist
 import argparse
 
+
 def get_args():
     # Define the arguments we need to pass to dist.script_wrapper
     parser = argparse.ArgumentParser()
-    parser.add_argument('--backend', 
-                        type=str, 
+    parser.add_argument('--backend',
+                        type=str,
                         default='nccl',
                         choices=['nccl', 'gloo'],
                         help='Communication backend (nccl or gloo)')
-    parser.add_argument('--timeout', 
-                        type=int, 
+    parser.add_argument('--timeout',
+                        type=int,
                         default=30,
                         help='Time before process is killed in seconds')
-    parser.add_argument('--nccl_p2p_disable', 
-                        type=int, 
+    parser.add_argument('--nccl_p2p_disable',
+                        type=int,
                         default=1,
-                        choices=[0,1],
-                        help='Disable (1) or enable (0) NCCL peer-to-peer communication')
+                        choices=[0, 1],
+                        help='Disable (1) or enable (0) NCCL peer-to-peer communication') # noqa
     parser.add_argument('--seed',
                         type=int,
                         default=None,
                         help='Sets the RNG seed for all devices')
-    parser.add_argument('script_path', 
-                        type=str, 
-                        help='Single GPU script file name (with or without .py extension)')
-    
+    parser.add_argument('script_path',
+                        type=str,
+                        help='Single GPU script file name')
+
     return parser.parse_args()
 
 
@@ -51,10 +55,10 @@ def main():
     args = get_args()
     # Pass arguments to dist.script_wrapper
     dist.run_single_gpu_script(script_path=args.script_path,
-                                backend=args.backend,
-                                timeout=args.timeout,
-                                nccl_p2p_disable=bool(args.nccl_p2p_disable),
-                                seed=args.seed)
+                               backend=args.backend,
+                               timeout=args.timeout,
+                               nccl_p2p_disable=bool(args.nccl_p2p_disable),
+                               seed=args.seed)
 
 
 if __name__ == '__main__':
