3c8de5dc192072fdc5e0753d00183e3c31cca41f
Abe Levitan <abraham.levitan@psi.ch>
2025-10-16 14:16:12 +0200

    Update the example codes

--- examples/fancy_ptycho.py
+++ examples/fancy_ptycho.py
@@ -19,19 +19,31 @@ device = 'cuda'
 model.to(device=device)
 dataset.get_as(device=device)
 
+# Now, to do the reconstruction we will use the more flexible pattern of
+# creating an explicit reconstructor. This is what is used behind the
+# scenes by the convenience functions model.<algorithm>_optimize. For a long
+# reconstruction script with multiple steps, it is better to create the
+# reconstructor explicitly.
+#
+# The reconstructor will store the model and dataset and create an appropriate
+# optimizer. This allows the optimizer to persist, along with e.g. estimates
+# of the moments of individual parameters between loops
+recon = cdtools.reconstructors.AdamReconstructor(model, dataset)
+
 # The learning rate parameter sets the alpha for Adam.
 # The beta parameters are (0.9, 0.999) by default
 # The batch size sets the minibatch size
-for loss in model.Adam_optimize(50, dataset, lr=0.02, batch_size=10):
+for loss in recon.optimize(50, lr=0.02, batch_size=10):
     print(model.report())
     # Plotting is expensive, so we only do it every tenth epoch
     if model.epoch % 10 == 0:
         model.inspect(dataset)
 
 # It's common to chain several different reconstruction loops. Here, we
-# started with an aggressive refinement to find the probe, and now we
-# polish the reconstruction with a lower learning rate and larger minibatch
-for loss in model.Adam_optimize(50, dataset,  lr=0.005, batch_size=50):
+# started with an aggressive refinement to find the probe in the previous
+# loop, and now we polish the reconstruction with a lower learning rate
+# and larger minibatch
+for loss in recon.optimize(50, lr=0.005, batch_size=50):
     print(model.report())
     if model.epoch % 10 == 0:
         model.inspect(dataset)
